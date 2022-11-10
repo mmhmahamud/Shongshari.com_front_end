1d1b76a9e2ac2eba7549107f67a285cdb2d59126
Md Robin Hossain <hossainmdrobin967@gmail.com>
2022-11-10 15:56:09 -0600

    swap and match edited

--- src/components/homePage/bannerSection/SwipeMatch.jsx
+++ src/components/homePage/bannerSection/SwipeMatch.jsx
@@ -5,99 +5,11 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import FollowServices from "../../../api/FollowServices";
 import { toastifyAlertError, toastifyAlertSuccess } from "../../toast/toast";
 import WithoutLoginSwipeMatch from "./WithoutLoginSwipeMatch";
+import dummyData from "../topProfile/dummyData";
+
 function SwipeMatch() {
-  const { authenticated } = useSelector((state) => state.authValue);
-
-  const [swipeValue, setswipeValue] = useState();
-  const swipeMatchFun = async () => {
-    let res = await FollowServices.swipeMatch();
-    if (res.status === 200) {
-      setswipeValue(res.data);
-    }
-  };
-  useEffect(() => {
-    swipeMatchFun();
-  }, []);
-
-  const refreshed = () => {
-    toastifyAlertSuccess("Refreshed", "top-right");
-    // dispatch(swapSuggestData());
-    swipeMatchFun();
-  };
-
-  const addToFavourite = async (id, type) => {
-    let data = {
-      type: type,
-      favourite_id: id,
-    };
-    let res = await FollowServices.addFavourite(data);
-    //console.log("res :>> ", res);
-    if (res.status === 201) {
-      toastifyAlertSuccess("Added", "top-center");
-      swipeMatchFun();
-    } else {
-      toastifyAlertError(res.data.message, "top-center");
-    }
-  };
-  // console.log("object :>> ", swipeValue);
-  let swapMatch = "";
-  if (swipeValue?.data) {
-    swapMatch = swipeValue.data.map((swap, index) => (
-      <SwiperSlide>
-        <div key={index} className="swiper-slide banner-img-item">
-          <img className="img-fluid" src={swap.profile_photo} alt="" />
-          <div className="swipe-overly">
-            <div className="swipe-bottom">
-              <div className="swipe-details">
-                <div className="row">
-                  <div className="col-md-10">
-                    <h2>
-                      {swap.name} <samp>{swap.age}</samp>
-                    </h2>
-                    <p className="swipe-label1">
-                      <i className="fas fa-star"></i> {swap.city}
-                    </p>
-                    <p className="swipe-label2">
-                      <i className="fas fa-star"></i> {swap.country}
-                    </p>
-                  </div>
-                  <div className="col-md-2">
-                    <a className="swipe-info rounded-circle" href="">
-                      <i className="fas fa-info"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <div className="swipe-option">
-                <a className="rounded-circle">
-                  <i className="fas fa-undo" onClick={refreshed}></i>
-                </a>
-
-                <a
-                  className="rounded-circle"
-                  onClick={() => addToFavourite(swap.id, 1)}
-                >
-                  <i className="fas fa-star"></i>
-                </a>
-                <a
-                  className="rounded-circle"
-                  onClick={() => addToFavourite(swap.id, 2)}
-                >
-                  <i className="fas fa-heart"></i>
-                </a>
-                <a
-                  className="rounded-circle"
-                  onClick={() => addToFavourite(swap.id, 3)}
-                >
-                  <i className="fas fa-bolt"></i>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </SwiperSlide>
-    ));
-  }
+  const [match, setMatch] = useState(4);
+  
   return (
     <div className="col-xl-4 col-lg-5  col-md-6">
       <div className="swiper mySwiperImg">
@@ -113,7 +25,7 @@ function SwipeMatch() {
             modules={[Navigation]}
             className=" swiper-wrapper"
           >
-            {authenticated ? swapMatch : <WithoutLoginSwipeMatch />}
+            <WithoutLoginSwipeMatch match={match} setMatch={setMatch} item={dummyData[match]}/>
           </Swiper>
         </div>
         <div className="swipe-header">
