389eb6bc2bce43d5d2f5d1450a0d06c544fde957
Md Robin Hossain <hossainmdrobin967@gmail.com>
2022-11-09 19:58:07 -0600

    dummy data created

---
+++ src/components/homePage/topProfile/dummyData.js
@@ -0,0 +1,95 @@
+import ali from './dummyImg/ali.png'
+import meem from "./dummyImg/meem.png";
+import sanjida from "./dummyImg/sanjida.png";
+import shahriar from "./dummyImg/shahriar.png";
+import shakil from "./dummyImg/shakil.png";
+import tuhidul from "./dummyImg/tuhidul.png";
+import tumpa from "./dummyImg/tumpa.png";
+
+const dummyData = [
+  {
+        name: "Sajia Jahan Rakha",
+      img:sanjida,
+    age: 22,
+    hight: "5 feet 4",
+    profession: "Student",
+    education: " Hon's(Department of Marketing)",
+    institution: "B.M college Barishal",
+    father: " Md. Kamal Khan",
+    mother: "Salma Begum",
+    address: "Barishal Sahadar Barishal",
+  },
+  {
+    name: "Md. Sahriar Saiful",
+    age: 26,
+    hight: "5 feet 6",
+    profession: "private holder",
+    education: "Hon’s ( deparment of physics )",
+    institution: "Dhaka college",
+    father: "Md. Manik Khan",
+      mother: "Anjira Begum",
+    img: shahriar
+  },
+  {
+    name: "Md. Shakil Mridha",
+    age: 26,
+    hight: "5 feet 5",
+    profession: "private holder",
+    education: "Hon’s ( deparment of management )",
+    institution: "B.M college",
+    father: "Md. Nasir Mridha",
+    mother: "Shirin Akter",
+    address: "barishal sadar, charaicha, kornokathi",
+    img: shakil
+  },
+  {
+    name: "Md. Tohidul islam shohag",
+    age: 27,
+    hight: "5 feet 6",
+    profession: "Private holder",
+    education: " Hon's(Department of History)",
+    institution: "Dhaka college",
+    father: "Md. Babul Mridha",
+    mother: "Soniya begum",
+    address: "Barishal shadar, Barishal",
+    img: tuhidul
+  },
+  {
+    name: " Tumpa Akter Sathi",
+    age: 22,
+    hight: "5 feet 4",
+    profession: "Student",
+    education: " Hon's(Department of Physics)",
+    institution: "University of Barishal",
+    father: "Md. Moshiur Rahman",
+    mother: "Moyna Begum",
+    address: "Patuakhali,  Sobuj bag.",
+    img:tumpa
+  },
+  {
+    name: " Sah Ali isalam",
+    age: 26,
+    hight: "5 feet 6",
+    profession: "Private holder",
+    education: " Hon's(Department of Bangla)",
+    institution: "Barishal college",
+    father: "Md. Manik Miya",
+    mother: "Tara Begum",
+    address: "Zatrabari Dhaka",
+    img: ali
+  },
+  {
+    name: "Nowrin Meem",
+    age: 21,
+    hight: "5 feet 5",
+    profession: "Student",
+    education: " Hon's(Department of Physics)",
+    institution: "Barishal University",
+    father: "Md. Badruzzaman",
+    mother: "Runa jahan",
+    address: "Chadpur, Matlab",
+    img: meem
+  },
+];
+
+export default dummyData;
