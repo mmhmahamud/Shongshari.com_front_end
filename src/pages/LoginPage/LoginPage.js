dd01c0e7c8f7d3ac4671a78a2d59f07d26419f87
mobashirul-alam <mobashirulalamseam@gmail.com>
2022-11-12 12:11:29 +0600

    login feature implemented

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,15 +1,43 @@
 import React from "react";
 import appStore from "../../assets/img/appstore.webp";
 import pic1 from "../../assets/img/play-store-logo-nisi-filters-australia-11.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { auth } from "../../firebase";
+import { useLoginAsMemberMutation } from "../../redux/features/auth/authApi";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { userLoggedIn } from "../../redux/features/auth/authenticationSlices";
 
 const LoginPage = () => {
+  const dispatch = useDispatch();
+  const [loginAsMember, { data: index }] = useLoginAsMemberMutation()
+  const navigate = useNavigate();
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+
   const handleLogin = async (e) => {
     e.preventDefault();
 
     const data = new FormData(e.target);
     const result = Object.fromEntries(data.entries());
+    const { email, password } = result;
+
+    await signInWithEmailAndPassword(email, password);
+    await loginAsMember(result);
   };
+
+  useEffect(() => {
+    if (index) {
+      localStorage.setItem("accessToken", index.token);
+      navigate("/");
+      dispatch(userLoggedIn({ userCredentials: index, isauthenticated: true }));
+    }
+  }, [index, dispatch, navigate]);
 
   return (
     <>
@@ -41,7 +69,7 @@ const LoginPage = () => {
               </div>
 
               <div class="button">
-                <input type="submit" />
+                <input type="submit" value={`${loading ? 'Loading' : 'Login'}`} />
               </div>
               <div className="logiFooter">
                 <h6>Get the app</h6>
